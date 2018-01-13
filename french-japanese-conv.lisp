5b686a60f927c398ec34f601f436035ef88d8c07
Koji Matsuda <kjmatsuda1985@gmail.com>
2018-01-14 07:01:44 +0900

    - 出力ファイル名を指定できるようにした - 翻訳できなかった単語は別ファイルに残すようにした

--- french-japanese-conv.lisp
+++ french-japanese-conv.lisp
@@ -3,7 +3,7 @@
 (ql:quickload :clss)   ;; CSSセレクタ
 
 ;; usage
-;; (french-japanese-conv "in-test.txt")
+;; (french-japanese-conv "in-test.txt" "output.csv")
 ;; -> generate output.csv
 
 (defparameter *japanese-french-dic-url* "http://9.dee.cc/~hakase2/tokuken.php")
@@ -26,12 +26,27 @@
     sub-tree-text
     ))
 
-(defun french-japanese-conv (in-file)
+(defun french-japanese-conv (in-file out-file)
+  (let ((translated)
+        (french-word))
   (with-open-file (in-stream in-file)
-    (with-open-file (out-stream "output.csv" :direction :output :if-exists :supersede :external-format :utf8)
-      (loop for line = (read-line in-stream nil nil)
-            while line
-            do (progn
-                 (setq line (string-right-trim '(#\Return #\Newline) line))
-                 (format out-stream "~A,~A~%" line (get-japanese line))
-                 (sleep 1))))))
+    (with-open-file (out-stream out-file :direction :output :if-exists :append :if-does-not-exist :create :external-format :utf8)
+      (with-open-file (no-match-stream (concatenate 'string "no-match-" in-file ) :direction :output :if-exists :supersede :external-format :utf8)
+        (loop for line = (read-line in-stream nil nil)
+              while line
+              do (progn
+                   (setq french-word (string-right-trim '(#\Return #\Newline) line))
+                   (setq translated (get-japanese french-word))
+                   (if (not (string=  translated ""))
+                       (format out-stream "~A,~A~%" french-word translated)
+                       (format no-match-stream "~A~%" french-word))
+                   (sleep 1))))))))
+
+
+
+
+
+
+
+
+
